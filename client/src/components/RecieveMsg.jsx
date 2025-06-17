4d6c82d57ae973d412325e3c4079a0617e70971e
anandclicks <puspanand94@gmail.com>
2025-06-18 02:35:52 +0530

    fix

--- client/src/components/RecieveMsg.jsx
+++ client/src/components/RecieveMsg.jsx
@@ -8,14 +8,12 @@ const RecieveMsg = ({ msgData }) => {
     <>
       {msgData?.images?.length >= 1 && (
         <div className='sendMsgWrapper h-[200px] w-full flex justify-start items-center mb-2'>
-          <div className='bg-steal-500 max-w-[250px] min-w-[250px] shadow relative text-white text-sm h-full flex justify-center items-center rounded-3xl overflow-hidden'>
-            <img className='h-full object-cover' src={msgData?.images[0]} alt="" />
+            <img className='max-w-[250px] rounded-xl h-full object-contain' src={msgData?.images[0]} alt="" />
 
             {/* image number  */}
             {msgData?.images.length >= 2 && (
               <div className='bg-[#00000050] absolute z-10 h-full w-full flex justify-center items-center text-white text-xl cursor-pointer'>{msgData?.images.length - 1}+</div>
             )}
-          </div>
         </div>
       )}
 
